10ea3aff282403e13d2c7ad506c8fbdd13a5adfe
Jojocrazy-byui <hen21054@byui.edu>
2025-03-19 10:47:38 -0600

    hello...

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from modules import reading_csv
-p_e = reading_csv.print_excel()
 
-def cycle(check_list[]): # Works by checking if the inputs are in fact the inputs we are looking for.
+def cycle(): # Works by checking if the inputs are in fact the inputs we are looking for.
     choice = '-1'
-    while choice not in check_list[]
-    print("----CSE 111: Final Project----")
-    print("Written by Jacob Henrie, Spencer *******, Carson *******")
-    print("Personal Finance application")
-    print("1)N.A.\t2)N.A.\t3)N.A.\t0)Exit")
-    choice = input('->')
-    return 
-
+    while choice not in ['1', '2', '3', '0', '0x']:
+        print("----CSE 111: Final Project----")
+        print("Written by Jacob Henrie, Spencer Olson, Carson Payne")
+        print("Personal Finance application")
+        print("1)N.A.\t2)N.A.\t3)N.A.\t0)Exit")
+        choice = input('->')
+    return choice
+# 1) Check Account
+# 2)
+# 3)
 
 #This is main.
-validate_inputs = ['1', '2', '3', '0', '0x']
-choice = cycle(validate_inputs)
+choice = cycle()
 while choice != '0':
-    if choice == '1'
+    if choice == '1':
         pass
-    elif choice == '2'
+    elif choice == '2':
         pass
-    elif choice == '3'
+    elif choice == '3':
         pass
-    elif choice == '0x'
+    elif choice == '0x':
+        reading_csv.print_excel()
     choice = cycle()
