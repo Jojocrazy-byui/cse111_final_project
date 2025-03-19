f234b5260e8b047bb351f0a662fab49687aa28cd
SpencerO921 <ols24029@byui.edu>
2025-03-19 10:46:49 -0600

    Update main.py
    Names

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def cycle(check_list[]): # Works by checking if the inputs are in fact the input
     choice = '-1'
     while choice not in check_list[]
     print("----CSE 111: Final Project----")
-    print("Written by Jacob Henrie, Spencer *******, Carson *******")
+    print("Written by Jacob Henrie, Spencer Olson, Carson Payne")
     print("Personal Finance application")
     print("1)N.A.\t2)N.A.\t3)N.A.\t0)Exit")
     choice = input('->')
