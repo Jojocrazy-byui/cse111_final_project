94e5de0ded78052aae0c140eba507160e8e217e5
Jojocrazy-byui <hen21054@byui.edu>
2025-03-19 10:31:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from modules import reading_csv
 p_e = reading_csv.print_excel()
 
-def cycle():
+def cycle(check_list[]): # Works by checking if the inputs are in fact the inputs we are looking for.
+    choice = '-1'
+    while choice not in check_list[]
     print("----CSE 111: Final Project----")
+    print("Written by Jacob Henrie, Spencer *******, Carson *******")
     print("Personal Finance application")
     print("1)N.A.\t2)N.A.\t3)N.A.\t0)Exit")
-    return input('->')
+    choice = input('->')
+    return 
+
+
 #This is main.
-
-
-choice = cycle()
+validate_inputs = ['1', '2', '3', '0', '0x']
+choice = cycle(validate_inputs)
 while choice != '0':
-    print('Made it!')
+    if choice == '1'
+        pass
+    elif choice == '2'
+        pass
+    elif choice == '3'
+        pass
+    elif choice == '0x'
     choice = cycle()
