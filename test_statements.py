90ba0d2a97872b99ef337b2571aea955cb8ae831
Jojocrazy-byui <hen21054@byui.edu>
2025-04-07 12:32:12 -0600

    test code....?

---
+++ test_statements.py
@@ -0,0 +1,10 @@
+from . import main
+#File for testing 
+def test_menu():
+    assert main.cycle() == 'a'
+    assert main.cycle() == 1
+
+def test_budget():
+    pass
+
+# I have no clue on how to use this
